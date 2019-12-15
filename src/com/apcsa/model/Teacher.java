dcd18c972fca3383fe0150b406f45554b6ac5e80
ngoodman1021 <32653505+ngoodman1021@users.noreply.github.com>
2019-12-15 16:21:35 -0500

    8/50

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -3,6 +3,8 @@ package com.apcsa.model;
 import com.apcsa.model.User;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class Teacher extends User {
 
@@ -10,5 +12,16 @@ public class Teacher extends User {
     private int departmentId;
     private String firstName;
     private String lastName;
+
+    public Teacher(User user, ResultSet rs) throws SQLException {
+        super(user);
+        
+        this.teacherId = rs.getInt("teacher_id");
+        this.departmentId = rs.getInt("department_id");
+        this.firstName = rs.getString("first_name");
+        this.lastName = rs.getString("last_name");
+        this.departmentId = rs.getInt("department_id");
+    
+    }
 
 }
